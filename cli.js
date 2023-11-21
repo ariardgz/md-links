d3ab3e7de6c18f4a52cb2fdeb1f32a0e01c424f1
Ariadna Rodriguez <ariadna.rodriguez30@hotmail.com>
2023-11-21 10:22:28 -0600

    Hito 2 terminado con test corriendo mayor al 70%

--- cli.js
+++ cli.js
@@ -1,11 +1,12 @@
 const { mdLinks } = require('./index.js');
 
-mdLinks('examples/ejemplo1.md', true).then((response)=>{
+mdLinks('examples/linkError.md', true).then((response)=>{
     console.log(response);
 })
 .catch((error)=> {
     console.log(error);
 })
-
+// C:/Users/ariad/Documents/LABORATORIA/MD-LINKS/md-links/examples/linkError.md
+// 'examples/ejemplo1.md'
 // C:\Users\ariad\Documents\LABORATORIA\MD-LINKS\md-links\examples\ejemplo1.md 
 // C:/Users/ariad/Documents/LABORATORIA/MD-LINKS/md-links/examples/ejemplo1.md 
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,5 +1,5 @@
 const { mdLinks } = require('../index.js');
-const { findLinks } = require('../functions.js');
+const { findLinks, validatedLinks } = require('../functions.js');
 
 
 
@@ -32,7 +32,7 @@ describe('mdLinks', () => {
           file: path
         }
     ]
-    expect(mdLinks(path, {validate: false})).resolves.toEqual(links);
+    expect(mdLinks(path)).resolves.toEqual(links);
           
     });
 
@@ -46,12 +46,28 @@ describe('mdLinks', () => {
             file: 'C:\\Users\\ariad\\Documents\\LABORATORIA\\MD-LINKS\\md-links\\examples\\ejemplo1.md'
           }
       ]
-      expect(mdLinks(path, {validate: true})).resolves.toEqual(links);
+      expect(mdLinks(path)).resolves.toEqual(links);
             
       });
 
+      it('should return an array with href, text, file, status, message', ()=>{
+        const path = 'C:/Users/ariad/Documents/LABORATORIA/MD-LINKS/md-links/examples/ejemplo1.md'
+        const links = [
+          
+            {
+              href: 'https://es.wikipedia.org/wiki/Markdown',
+              text: 'Markdown',
+              file: path,
+              status: 200,
+              message: 'ok'
+            }
+        ]
+        expect(mdLinks(path)).resolves.toStrictEqual(expect.anything(links));
+        })
+      
 
 });
+
 describe('Read links', ()=>{
   it('should return all the link of the path', () =>{
     const content = 'Hola soy un archivo .md con texto [Markdown](https://es.wikipedia.org/wiki/Markdown)';
@@ -67,6 +83,69 @@ describe('Read links', ()=>{
     expect(findLinks(content, path)).toEqual(links);
           
     });
+
+    
+
+}),
+
+describe('Validate links', () => {
+  it('should be a function', () =>{
+    expect(typeof validatedLinks).toBe('function');
+  })
+
+  const arrResponse = [
+          
+    {
+      href: 'https://es.wikipedia.org/wiki/Markdown',
+      text: 'Markdown',
+      file: 'C:/Users/ariad/Documents/LABORATORIA/MD-LINKS/md-links/examples/ejemplo1.md',
+      status: 200,
+      message: 'ok'
+    }
+];
   
+  const links = [
+    
+      {
+        href: 'https://es.wikipedia.org/wiki/Markdown',
+        text: 'Markdown',
+        file: 'C:/Users/ariad/Documents/LABORATORIA/MD-LINKS/md-links/examples/ejemplo1.md'
+      }
+  ];
+  it('should return the status: 200 and message: ok', () =>{
+    validatedLinks(links, 'C:/Users/ariad/Documents/LABORATORIA/MD-LINKS/md-links/examples/ejemplo1.md').then(((response) => {
+      expect(response).toEqual(arrResponse);
+    }))
+          
+    });
+
+    const linksFail = [
+      {
+        href: 'https://es.wikipedia.org/wiki/Noe.js',
+        text: 'Node.js - Wikipedia',
+        file: 'C:/Users/ariad/Documents/LABORATORIA/MD-LINKS/md-links/examples/linkError.md',
+      }
+    ];
+
+    const arrResponseFail = [
+      {
+        href: 'https://es.wikipedia.org/wiki/Noe.js',
+        text: 'Node.js - Wikipedia',
+        file: 'C:/Users/ariad/Documents/LABORATORIA/MD-LINKS/md-links/examples/linkError.md',
+        status: 404,
+        message: 'fail'
+      }
+    ]
+
+    it('should return status: 404 and message: fail', () =>{
+      validatedLinks(linksFail, 'C:/Users/ariad/Documents/LABORATORIA/MD-LINKS/md-links/examples/linkError.md').then(((response) =>{
+        expect(response).toEqual(arrResponseFail);
+      }))
+    })
+
+
+
+    
 
 })
+
